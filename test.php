46e26c3865ac3773810da1b7768efc551a89d1f1
NetGodFather <hezhiqun@gmail.com>
2018-06-12 14:59:32 +0800

    Create test.php

---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+require './coinness/apiClient.php';
+
+use Coinness\apiClient;
+use const Coinness\COINNESS_LANGUAGE_KO;
+
+$app_id = ""; // Your App id;
+$app_sercet = ""; // Your Sercet
+
+$client = new apiClient($app_id, $app_sercet);
+
+$newsflash_list = $client->getNewsflashList(COINNESS_LANGUAGE_KO);
+
+var_dump($newsflas_list);
